11c30e858de29d1e83b9769f41e534f9d7393428
Charlie Park <charlie@pearbudget.com>
2010-12-18 23:10:14 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbjr::Application.config.secret_token = '15fff9ce32604bfa7e4dc6001a0a01da1a40c1b5f8179302d150180bacb5ed480cfc562c5f14cb21434110665d0574e3baae48787be47d88f8aac4b67328fb75'
