042583f558056c49086c3b05e097990d2e89e503
Charlie Park <charlie@pearbudget.com>
2010-12-27 01:05:15 -0500

    Adds a few more boxes to the page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+jQuery.fn.onloadEvents = function() {
+	$('#example').red();
+	
+};
+
+jQuery.fn.red = function() {
+	$(this).css('background','red');
+}
