cd1f2da5848610d4927edca3f003311d12632d64
Charlie Park <charlie@pearbudget.com>
2010-12-28 14:48:50 -0500

    Better info on admin box. Also, floating boxes are closable.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,3 +14,13 @@ $("#new_password_trigger").click(function(){
 $("#new_password_cancel").click(function(){
 	$("#new_password_form").fadeOut();
 });
+
+$(".close").click(function(){
+	$(this).parents(".floating_box").fadeOut();
+});
+
+$(".popup_trigger").click(function(e){
+	e.preventDefault();
+	var box = $(this).attr('href');
+	$("#"+box).fadeIn();
+});
